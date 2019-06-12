be404618b6543bbee3e3904f713513e9ceeeb054
Patricia Yukimi Kavamukai <patricia_yukimi@hotmail.com>
2019-06-12 01:30:27 -0300

    testes excluídos

--- spec/passo1_spec.rb
+++
@@ -1,9 +0,0 @@
-
-
-describe 'Acesso ao sistema' do
-
-   it 'Passo 1' do     
-        visit 'https://atech-airlines-ui-staging.herokuapp.com'
-        expect(page.title).to eql 'Atech Airlines'
-        end
-   end
